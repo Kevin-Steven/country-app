1f17f82f7cea260c4d26f0e5fba1de991af461c2
Kevin <kbarzola.istjba@gmail.com>
2025-07-30 18:52:45 -0500

    fix: resolve errors

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CountryList } from "../../components/country-list/country-list";
+import { SearchInput } from "../../components/search-input/search-input";
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [CountryList, SearchInput],
   templateUrl: './by-capital-page.html',
 })
 export class ByCapitalPage {
+
+  buscar( value: string ){
+    console.log({value})
+  }
 
 }
---
+++ src/app/country/pages/by-country-page/by-country-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SearchInput } from "../../components/search-input/search-input";
+import { CountryList } from "../../components/country-list/country-list";
+
+@Component({
+  selector: 'by-country-page',
+  imports: [SearchInput, CountryList],
+  templateUrl: './by-country-page.html',
+})
+export class ByCountryPage {
+
+}
